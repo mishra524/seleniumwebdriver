69de231df86623469f874b9ebb7161cd09323833
chandan.kumar <chandan.kumar@BLR-5CG63266S2>
2025-05-01 13:44:52 +0530

    This is Commit-3rdInstance-BranchCommit

---
+++ src/test/java/testpckg/TestCase_TC002.java
@@ -0,0 +1,14 @@
+package testpckg;
+
+public class TestCase_TC002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is a test case to validate branch commit!");
+		
+		
+	}
+
+}
